18eac87f214b81cc4d4f3c6f9bf131c52a08c5ee
Mack <macknz7@gmail.com>
2020-10-06 09:26:52 -0700

    Reverse String Output Added

--- JavascriptCodingExerciseHomework1/Scripts/CustomJS.js
+++ JavascriptCodingExerciseHomework1/Scripts/CustomJS.js
@@ -48,7 +48,9 @@ function FizzBuzz(fizz, buzz, upperLimit) {
 // Palindrome
 function Palindrome(word) {
     const e = document.getElementById("palindrome-result");
+    const r = document.getElementById("reversed-word");
     let reverse = word.split('').reverse().join('');
+    r.innerHTML = `<b>${reverse}</b>`;
     if (word.toLowerCase() == reverse.toLowerCase()) {
         e.innerHTML = `<b>Palindrome!</b>`;
     } else {
